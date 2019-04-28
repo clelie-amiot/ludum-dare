1ee2a189c1f38d8230d62068b4e72ca9f7c72856
BriceCl <briec.clement@gmail.com>
2019-04-28 14:56:19 +0200

    Added files of little robot and scene

---
+++ Assets/Scripts/fireBullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireBullet : MonoBehaviour {
+
+	public Transform firePoint;
+	public GameObject bulletPrefab;
+	public GameObject character;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			firePoint = character.transform;
+			firePoint.position.Set(character.transform.position.x + 0.2f, character.transform.position.y, character.transform.position.z);
+			Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+		}
+	}
+
+}
