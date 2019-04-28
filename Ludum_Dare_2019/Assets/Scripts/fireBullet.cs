d5772730b62ed6a6a9318015b12273691c0117de
brclemen <brice.clement@inria.fr>
2019-04-28 14:03:26 +0200

    Revert "added scrpit for bullet"
    This reverts commit 5c84faf21dcc3d062938372f548e3a2c5bf48cad.

--- Ludum_Dare_2019/Assets/Scripts/fireBullet.cs
+++ Ludum_Dare_2019/Assets/Scripts/fireBullet.cs
@@ -5,15 +5,14 @@ public class fireBullet : MonoBehaviour {
 
 	public Transform firePoint;
 	public GameObject bulletPrefab;
-	public GameObject character;
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("Fire1")) {
-			firePoint = character.transform;
-			firePoint.position.Set(character.transform.position.x + 0.2f, character.transform.position.y, character.transform.position.z);
-			Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-		}
+		if (Input.GetButtonDown("Fire1")) {
+			Shoot ();
 	}
 
+	void Shoot() {
+			Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+	}
 }
