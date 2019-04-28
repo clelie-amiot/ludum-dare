62c59a307d09eb6f42eb34d4074e48eaedc8954b
C Amiot <amiot.clelie@gmail.com>
2019-04-28 15:41:41 +0200

    merchant prefab and scripts

---
+++ Assets/Scripts/onApproach.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class onApproach : MonoBehaviour
+{
+    public GameObject merchant;
+    public GameObject bubble;
+    public GameObject player;
+
+    public float minDistance = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        merchant = GameObject.Find("Merchant");
+        bubble = GameObject.Find("MerchantBubble");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        player = GameObject.Find("CharacterRobotBoy");
+
+        float distance = Vector3.Distance(player.transform.position, merchant.transform.position);
+        bubble.SetActive(distance < minDistance);
+    }
+}
---
+++ Assets/Scripts/playerHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;                            // The amount of health the player starts the game with.
+    public int currentHealth;                                   // The current health the player has.
+    public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
