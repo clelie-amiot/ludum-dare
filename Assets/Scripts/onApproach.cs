d8f8d239f5c713716b16d7c34ce092087de404c2
C Amiot <amiot.clelie@gmail.com>
2019-04-28 15:48:45 +0200

    update merchant

--- Assets/Scripts/onApproach.cs
+++ Assets/Scripts/onApproach.cs
@@ -13,15 +13,11 @@ public class onApproach : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        merchant = GameObject.Find("Merchant");
-        bubble = GameObject.Find("MerchantBubble");
     }
 
     // Update is called once per frame
     void Update()
     {
-        player = GameObject.Find("CharacterRobotBoy");
-
         float distance = Vector3.Distance(player.transform.position, merchant.transform.position);
         bubble.SetActive(distance < minDistance);
     }
