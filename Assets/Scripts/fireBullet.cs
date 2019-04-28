bbb2f29df3e5e70877670d91dc34cb85c2b04660
BriceCl <briec.clement@gmail.com>
2019-04-28 16:33:52 +0200

    Add music and sound effects

--- Assets/Scripts/fireBullet.cs
+++ Assets/Scripts/fireBullet.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class fireBullet : MonoBehaviour {
 
 	public Transform firePoint;
+    public AudioSource shot;
 	public GameObject bulletPrefab;
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1")) {
             Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            shot.Play();
 		}
 	}
 
