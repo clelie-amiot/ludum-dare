f88b13897a15e2817ff217d6db3c4e0365707d53
BriceCl <briec.clement@gmail.com>
2020-01-14 19:09:50 +0100

    lsat update

---
+++ Assets/Scripts/healthbar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    public GameObject healthbarPrefab;
+    private Scrollbar scrollb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrollb = healthbarPrefab.GetComponent<Scrollbar>();
+    }
+
+    void ApplyDamages()
+    {
+        scrollb.size -= 0.1f;
+    }
+}
